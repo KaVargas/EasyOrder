e8890405c73e19dd2e377ace8dcf46c419717319
Andres Ayala <andres.ayala@outlook.com>
2018-07-12 21:50:28 -0500

    Updated Entities.

---
+++ Code/EasyOrder/EasyOrderWeb/Models/Empleado.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EasyOrderWeb.Models
+{
+    public partial class Empleado
+    {
+        public Empleado()
+        {
+            Orden = new HashSet<Orden>();
+        }
+
+        public Guid Idpersona { get; set; }
+        public Guid Idempleado { get; set; }
+        public Guid Idrestaurante { get; set; }
+        public string Password { get; set; }
+        public string Username { get; set; }
+
+        public Persona IdpersonaNavigation { get; set; }
+        public Restaurante IdrestauranteNavigation { get; set; }
+        public ICollection<Orden> Orden { get; set; }
+    }
+}

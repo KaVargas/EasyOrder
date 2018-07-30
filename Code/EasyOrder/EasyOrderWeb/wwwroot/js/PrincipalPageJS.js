31505bc113a7514c99987b8a7bdc4f7f97dcc34c
kenedy777 <kenedyvargas777@hotmail.com>
2018-07-30 14:59:25 -0500

    Refactor of PrincipalPage.cshtml and RegisterPage.cshtml

---
+++ Code/EasyOrder/EasyOrderWeb/wwwroot/js/PrincipalPageJS.js
@@ -0,0 +1,24 @@
+﻿function MostrarComidas() {
+    document.getElementById("comidas").style.display = "block";
+}
+function OcultarComidas() {
+    document.getElementById("comidas").style.display = "none";
+}
+
+function MostrarBebidas() {
+    document.getElementById("bebidas").style.display = "block";
+}
+function OcultarBebidas() {
+    document.getElementById("bebidas").style.display = "none";
+}
+
+
+function Mostrar_Comidas() {
+    OcultarBebidas();
+    MostrarComidas();
+}
+
+function Mostrar_Bebidas() {
+    OcultarComidas();
+    MostrarBebidas();
+}
---
+++ Code/EasyOrder/EasyOrderWeb/wwwroot/js/RegisterPageJS.js
@@ -0,0 +1,57 @@
+﻿
+function validateForm() {
+    $.ajax({
+        url: "/api/user/register",
+        method: "POST",
+        data: JSON.stringify({
+            UserName: $("#nickname").val(),
+            Password: $("#password").val(),
+            FullName: $("#fullname").val(),
+            CI: $("#ci").val(),
+            PhoneNumber: $("#phone").val()
+        }),
+        dataType: 'json',
+        contentType: "application/json",
+        async: false,
+        success: function (result, status, jqXHR) {
+            if (result.allowed) {
+                targetUrl.value = "/Index";
+            }
+            else {
+                targetUrl.value = "/RegisterPage";
+
+            }
+        },
+        error(jqXHR, textStatus, errorThrown) {
+            targetUrl.value = "/RegisterPage";
+        }
+    });
+}
+
+function validateCI() {
+    $.ajax({
+        url: "/api/user/validateci",
+        method: "POST",
+        data: JSON.stringify({
+            CI: $("#ci").val()
+        }),
+        dataType: 'json',
+        contentType: "application/json",
+        async: false,
+        success: function (result, status, jqXHR) {
+            if (result.allowed) {
+                validateForm();
+                targetUrl.value = "/RegisterPage2";
+            }
+            else {
+                targetUrl.value = "/Error";
+                targetUrl.value = "/Error";
+                targetUrl.value = "/Error";
+
+            }
+        },
+        error(jqXHR, textStatus, errorThrown) {
+            targetUrl.value = "/RegisterPage";
+        }
+    });
+}
